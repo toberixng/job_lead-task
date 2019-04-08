741a8e208b12adb28bcc8ea44c193b4c28d3a915
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2019-04-08 10:29:46 -1200

    button dis appers settings

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ function openForm() {
 function closeForm() {
   document.getElementById("popBox").style.display = "none";
 }
+
+
+/* This block of code makes the button to disappear when it is clicked on 
+and reappear when the close icon on the pop up box is clicked on */
+$(document).ready(function(){
+  $(".open-button").click(function(){
+    $(this).hide();
+  });
+  $(".close").click(function(){
+    $(".open-button").show();
+  });
+});
