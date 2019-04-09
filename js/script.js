ac209694bd845d9a08c0e2f22debe8fca26c71a9
toberixng <tobyfatumo@gmail.com>
2019-04-09 20:12:11 +0100

    animating buttons

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-function openForm() {
+function openBox() {
   document.getElementById("popBox").style.display = "block";
 }
 
-function closeForm() {
+function closeBox() {
   document.getElementById("popBox").style.display = "none";
 }
 
 
 /* This block of code makes the button to disappear when it is clicked on 
 and reappear when the close icon on the pop up box is clicked on */
-$(document).ready(function(){
+$(function(){
   $(".open-button").click(function(){
     $(this).hide();
   });
   $(".close").click(function(){
-    $(".open-button").show();
+    $(".open-button").show().addClass('animated fadeInUp');
   });
 });
+
+$(function(){
+
+$("button.open-button").on('click', function(){
+  
+  $('.right-side-pop').addClass('animated bounceInRight');
+
+});
+});
+
+
+
+
